c5726c9f6af1fd82c336d6e8112937de895cf7ad
Chelsea Farley <chelseafarley91@gmail.com>
2022-03-03 20:29:31 +1300

    Alert tutorial

--- App.js
+++ App.js
@@ -1,10 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Alert, View } from 'react-native';
 
 export default function App() {
+  let twoButtonAlert = () => {
+    Alert.alert("Hi there", "This is a message",
+      [ 
+        {
+          text: "Cancel",
+          onPress: () => {
+            console.log("Do nothing because we cancelled");
+          }
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            console.log("Do whatever ok is in your app e.g. save/delete");
+          }
+        }
+      ]
+    )
+  }
+
+  let threeButtonAlert = () => {
+    Alert.alert("Hi there", "This is a message",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            console.log("Do nothing because we cancelled");
+          }
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            console.log("Do whatever ok is in your app e.g. save/delete");
+          }
+        },
+        {
+          text: "Crazy Option",
+          onPress: () => {
+            console.log("Are you crazy? You chose the crazy option!");
+          }
+        }
+      ]
+    )
+  }
+
+  let prompt = () => {
+    Alert.prompt("Title", "Enter your name",
+      (name) => console.log(`Hi ${name}`)
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title="2 Button Alert"
+        onPress={twoButtonAlert}
+      />
+      <Button 
+        title="3 Button Alert"
+        onPress={threeButtonAlert}
+      />
+      <Button 
+        title="Prompt"
+        onPress={prompt}
+      />
       <StatusBar style="auto" />
     </View>
   );
